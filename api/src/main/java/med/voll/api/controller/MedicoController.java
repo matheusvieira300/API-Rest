16a64ac80950834cee2c7799aab36f465a0c0548
matheusvieira300 <53275513+matheusvieira300@users.noreply.github.com>
2023-07-22 20:12:13 -0300

    Feito o C R U falta o D

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,15 +3,15 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -27,5 +27,19 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){//pageable classe do spring para paginação
+        //@PageableDefault caso não for passado nenhum parametro na URL para realizar o padrão das páginas
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);// metodo listar convertendo uma lista de médico
+        //para DadosListagemMedico
+    }
+
+    @PutMapping // putmapping é comumente utiizado para atualização
+    @Transactional //definindo que está sendo feita uma transação no banco de dados
+    public void atualizar(@RequestBody @Valid DadosAtualicaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);// chamando o método para atualizar os dados baseado no DTO
+
+    }
 
 }
